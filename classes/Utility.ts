e0a04aa70de9d78af90dccc6f33005ff5f0b214b
mrezavasebi <reza.vaseb@gmail.com>
2024-04-08 20:05:20 +0330

    fixed: inputValues problem fixed

--- classes/Utility.ts
+++ classes/Utility.ts
@@ -1,9 +1,4 @@
 export class Utility {
-  validateFields(value: string | undefined): boolean {
-    if (value) if (value.trim().length !== 0) return true;
-    return false;
-  }
-
   logValue(msg: string, value: any): void {
     console.log(msg, JSON.stringify(value, null, 2));
   }
@@ -14,8 +9,7 @@ export class Utility {
     if (Object.keys(fields).length === 0) isValid = false;
     else {
       for (const key in fields) {
-        if (this.validateFields(fields[key as keyof typeof fields]))
-          isValid = false;
+        if (fields[key as keyof typeof fields] === "") isValid = false;
 
         if (!isValid) break;
       }
